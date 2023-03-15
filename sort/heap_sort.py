d57c033e9a05bf6f0a898b29571d262e7628e8be
kxmilkx <57270771+kxmilkx@users.noreply.github.com>
2023-03-15 21:59:38 +0100

    working heapsort

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -1,11 +1,30 @@
 def heapify(T, i, n):
-
     l = 2*i + 1
     r = 2*i + 2
 
     max_i = i
     
-    if l < n and T[l] > T[i]:
+    if l < n and T[l] > T[max_i]:
         max_i = l
+    if r < n and T[r] > T[max_i]:
+        max_i = r
+    
+    if max_i != i:
+        T[i], T[max_i] = T[max_i], T[i]
+        heapify(T, max_i, n)
 
-def heapsort(T):
+def buildHeap(T):
+    n = len(T)
+    for i in range((n-1)//2, -1, -1):
+        heapify(T, i, n)
+        
+
+def heapSort(T):
+    n = len(T)
+    buildHeap(T)
+
+    for i in range(n-1, 0, -1):
+
+        T[i], T[0] = T[0], T[i]
+        heapify(T, 0, i)
+
