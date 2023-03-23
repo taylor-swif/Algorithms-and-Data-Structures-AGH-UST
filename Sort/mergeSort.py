a1ebc4bbdd13e1d187b6053f12fc7d1a63bca063
kxmilkx <57270771+kxmilkx@users.noreply.github.com>
2023-03-23 14:43:59 +0100

    mergeSort

---
+++ Sort/mergeSort.py
@@ -0,0 +1,45 @@
+def merge(T, p, q ,r):
+    A = T[p:r + 1]
+
+    r = r - p
+    q = q - p
+
+    i = 0
+    j = q + 1
+    k = p
+    
+    while i < q + 1 and j < r + 1:
+        if A[i] <= A[j]:
+            T[k] = A[i]
+            i += 1
+        elif A[i] > A[j]:
+            T[k] = A[j]
+            j += 1
+        k += 1
+    
+    while i < q + 1:
+        T[k] = A[i]
+        i += 1
+        k += 1
+    
+    while j < r + 1:
+        T[k] = A[j]
+        j += 1
+        k += 1
+
+
+
+
+def mergeSort(T, p, r):
+    if p == r:
+        return
+    
+    q = (p + r)//2
+
+    mergeSort(T, p, q)
+    mergeSort(T, q + 1, r)
+    merge(T, p, q, r)
+
+T = [2, 1, 4, 5, 1, 6, 7, 9, 12, 3, 4]
+mergeSort(T, 0, len(T) - 1)
+print(T)
