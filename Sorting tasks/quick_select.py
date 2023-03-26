a91a0e111a7ce4378233cc8a23c7651c60273fc5
kxmilkx <57270771+kxmilkx@users.noreply.github.com>
2023-03-26 14:26:43 +0200

    quick select

---
+++ Sorting tasks/quick_select.py
@@ -0,0 +1,40 @@
+# kth order statistic
+
+def partitionLomuto(T, p, r):
+    x = T[r]
+
+    i = p - 1
+    q = p
+
+    while i < r:
+        i += 1
+        if T[i] < x:
+            T[i], T[q] = T[q], T[i]
+            q += 1
+    
+    T[r], T[q] = T[q], T[r]
+    
+
+    return q
+
+
+
+def quickSelect(T, k, p, r):
+
+    if p < r:
+
+        q = partitionLomuto(T, p, r)
+        if q == k:
+            print(q)
+            print(T)
+            return T[q]
+        elif q < k:
+            return quickSelect(T, k, q + 1, r)
+        else:
+            return quickSelect(T, k, p, q - 1)
+
+T = [0,2,1,6,3,5,9,7,8,4,10]
+
+print(quickSelect(T, 3, 0, len(T) - 1))
+
+    
