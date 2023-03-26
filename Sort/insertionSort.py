beaad6a64388fabdcbd0b124a6eb7b05552e8f9d
kxmilkx <57270771+kxmilkx@users.noreply.github.com>
2023-03-26 16:58:06 +0200

    insertion sort

--- Sort/insertionSort.py
+++ Sort/insertionSort.py
@@ -1,7 +1,17 @@
-def insertionSort(T):
+def beta_insertionSort(T):
     n = len(T)
     if T[0] > T[1]:
         T[0], T[1] = T[1], T[0]
+
+    for i in range(1, n - 1):
+        if T[i+1] < T[i]:
+            k = i + 1
+            while k > 0 and T[k - 1] > T[k]:
+                T[k - 1], T[k] = T[k], T[k - 1]
+                k -= 1
+
+def insertionSort(T):
+    n = len(T)
 
     for i in range(1, n - 1):
         if T[i+1] < T[i]:
