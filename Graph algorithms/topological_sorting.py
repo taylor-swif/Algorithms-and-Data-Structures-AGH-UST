277d663565c8ba0fadeceefe6b2f7e6d1baa8b86
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-04-15 17:41:21 +0200

    topological sorting

---
+++ Graph algorithms/topological_sorting.py
@@ -0,0 +1,29 @@
+# Topological sorting, sorting for directed acyclic graph
+# It is a linear ordering of its vertices that for every vertex you can only go right
+# Based on DFS
+G = [[1,2], [2, 3], [], [4, 5], [], [], [3]]
+
+def top_sort(G):
+    n = len(G)
+    visited = [False for v in range(n)]
+    parent = [None for v in range(n)]
+    sort = []
+    time = 0
+    
+    def DFSVisit(G, u):
+        nonlocal time
+        time += 1
+        visited[u] = True
+        for v in G[u]:
+            if not visited[v]:
+                parent[v] = u
+                DFSVisit(G, v)
+        sort.append(u)
+        time += 1
+
+    for u in range(n):
+        if not visited[u]:
+            DFSVisit(G, u)
+    print(sort[::-1])
+
+top_sort(G)
