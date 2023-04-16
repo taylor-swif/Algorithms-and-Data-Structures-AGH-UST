4d6645782849f951a3ba1ad7b1b389b02f825e11
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-04-17 00:48:25 +0200

    Update 06_Classes.py

--- Classes/06_Classes.py
+++ Classes/06_Classes.py
@@ -1,6 +1,6 @@
-#1 sprawdzanie czy graf jestr dwudzielny BFS
 from queue import Queue
 
+#1 sprawdzanie czy graf jestr dwudzielny BFS
 def is_biparie(G, s):
     n = len(G)
     Q = Queue(0)
@@ -19,8 +19,8 @@ def is_biparie(G, s):
                 if color[v] == color[u]:
                     return False
     return True
-# lista spojnych skladowych w grafie DFS
-#strongly connected components
+# 2 liczba spojnych skladowych w grafie DFS
+# strongly connected components
 def scc(G):
     vis = [0]*len(G)
     def DFS(v):
@@ -35,9 +35,9 @@ def scc(G):
             DFS(i)
     return l
 
-# stacje nadwacze odłączanie
-
-#dany jest graf G jako macierz sąsiedztwa, czy w tym grafie istnieje cykl długosci 4, składajaący się dokładnie z 4 wierzchłów?
+# 3 stacje nadwacze odłączanie
+#TODO
+# 4 dany jest graf G jako macierz sąsiedztwa, czy w tym grafie istnieje cykl długosci 4, składajaący się dokładnie z 4 wierzchłów?
 
 # O(n^4)
 # O(n^3)
@@ -53,32 +53,61 @@ def cykl4(G):
                 return True
             
     return False
-#graf skierowany
-# znaleźć uniwersalne wyjścia t
-# a dla kadego b 
 
-# zdj
+# 5 Graf skierowany G, jako macierz sasiedztwa
+# Znalezć uniwersalne ujście t
+# a) dla kazdego v istnieje krawedz z v do t
+# b) t nie posiada krawedzi wychodzacych
+#TODO
 
+G = [[0, 1, 0, 1, 0],
+     [0, 0, 0, 1, 0],
+     [1, 0, 0, 1, 1], 
+     [0, 0, 0, 0, 0], 
+     [0, 0, 0, 1, 0]]
+
+def exit(G):
+    n = len(G)
+    i = 0
+    j = 0
+
+    while j < n and i < n:
+        if G[i][j] == 0:
+            j += 1
+        elif G[i][j] == 1:
+            i += 1
+    for k in range(n):
+        if G[k][i] == 0:
+            if k != i:
+                return None
+    return i
+
+print(exit(G))
+# 6 zdj
 # szukamy najprostrzej siezki w grafie nieskierowanym bfs
+#TODO
 
-def s(G, s, t):
+def spath(G, s, t):
     vis = [0]*len(G)
     par = [-1]*len(G)
     g = Queue(0)
     # vis[]
 
-# dana jest szachownica nxn
+# 7 dana jest szachownica nxn
 #kazde pole ma wart. = {1, 2, 3, 4, 5}
 #chcemy w tajtańszy sposób przejsc z lewego gornego do prawego dolnego
 # szukany: minimalny koszt
+#TODO
 
-# zadanie 7
+# zadanie 8
 #malejące krawędzie
 # G = (V,E)
 # koszt {1, ..., |E|} kazda krawedz ma inny koszt
 #chcemy przejsc z krawdzi od s -> t
 # koszt jest malejący
 # dfs
+#TODO
 
-#zadanie 8
+#zadanie 9
 # graf to jest mapa drogowa kraju i chcemy przejechac jak najtaniej, gdzie koszt przejazdu to moze byc 0 lub 1
+#TODO
