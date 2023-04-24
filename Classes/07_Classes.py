75eb8eb365ec4e630c018928b11890dbc7c7786d
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-04-24 22:58:26 +0200

    Create 07_Classes.py

---
+++ Classes/07_Classes.py
@@ -0,0 +1,6 @@
+# 1 graf -dag czy istnieje sciezka hamiltona
+# 2 wierzchłek v jest dobrym początkiem jeśli z v mozna osiągnąć kazdy inny wierzcholek w grafie skierowanym
+# 3 znalezienie trasy z maksymalną przepustowościa
+# 4 znajdowanie cyklu eulera dla grafu w rep. macierzowej
+# 5 szachuje oazy
+
