a6041a43f0bdd616dd9b7a62e5cdbd8dccaaa404
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-04-24 22:58:17 +0200

    Create bridges.py

---
+++ Graph algorithms/bridges.py
@@ -0,0 +1,54 @@
+# undirected connected graph
+
+# G = [
+#     [0,1,1,0,0,0,0,0], 
+#     [1,0,0,0,1,0,0,0], 
+#     [1,0,0,1,1,0,0,0], 
+#     [0,0,1,0,0,0,0,0], 
+#     [0,1,1,0,0,1,0,0],
+#     [0,0,0,0,1,0,1,1],
+#     [0,0,0,0,0,1,0,1],
+#     [0,0,0,0,0,1,1,0]]
+G = [[1, 2], [0, 4], [0, 3, 4], [2], [1, 2, 5], [4, 6, 7], [5, 7], [5, 6]]
+G =  [[1, 2], [0, 3], [0, 4], [1, 5, 6], [2, 7], [3, 8], [3, 8], [4, 8], [5, 6, 7, 9], [8, 10, 11], [9, 12], [9, 12], [10, 11]]
+
+def bridge(G):
+    n = len(G)
+    visited = [False for v in range(n)]
+    parent = [None for v in range(n)]
+    time_arr = [-1 for v in range(n)]
+    low = [-1 for v in range(n)]
+    time = 0
+
+
+    def DFSVisit(G, s):
+        nonlocal time
+        visited[s] = True
+        time_arr[s] = time
+        low[s] = time
+        time += 1
+
+        for u in G[s]:
+
+            if u == parent[s]:
+                continue
+
+            if visited[u]:
+                low[s] = min(low[s], time_arr[u])
+
+            if not visited[u]:
+                parent[u] = s
+                DFSVisit(G, u)
+                low[s] = min(low[s], low[u])
+                if low[u] > time_arr[s]:
+                    print(s, u)
+                
+
+    for i in range(n):
+        if not visited[i]:
+            DFSVisit(G, i)
+    # print(parent)
+    # print(time_arr)
+    # print(low)
+
+bridge(G)
