1bbb501950415f417862e785263d7f5bdc7036d1
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-05-10 12:17:41 +0200

    bridges searching

--- Graph algorithms/bridges.py
+++ Graph algorithms/bridges.py
@@ -1,5 +1,6 @@
 # undirected connected graph
-
+# bridge is defined as edge that when it is removed makes the graph unconnected 
+# output: bridges 
 # G = [
 #     [0,1,1,0,0,0,0,0], 
 #     [1,0,0,0,1,0,0,0], 
@@ -9,17 +10,17 @@
 #     [0,0,0,0,1,0,1,1],
 #     [0,0,0,0,0,1,0,1],
 #     [0,0,0,0,0,1,1,0]]
-G = [[1, 2], [0, 4], [0, 3, 4], [2], [1, 2, 5], [4, 6, 7], [5, 7], [5, 6]]
+# G = [[1, 2], [0, 4], [0, 3, 4], [2], [1, 2, 5], [4, 6, 7], [5, 7], [5, 6]]
 G =  [[1, 2], [0, 3], [0, 4], [1, 5, 6], [2, 7], [3, 8], [3, 8], [4, 8], [5, 6, 7, 9], [8, 10, 11], [9, 12], [9, 12], [10, 11]]
+
 
 def bridge(G):
     n = len(G)
-    visited = [False for v in range(n)]
     parent = [None for v in range(n)]
     time_arr = [-1 for v in range(n)]
+    visited = [False for v in range(n)]
     low = [-1 for v in range(n)]
     time = 0
-
 
     def DFSVisit(G, s):
         nonlocal time
@@ -28,27 +29,61 @@ def bridge(G):
         low[s] = time
         time += 1
 
-        for u in G[s]:
+        for v in G[s]:
+            if not visited[v]:
+                parent[v] = s
+                DFSVisit(G, v)
+                low[s] = min(low[s], low[v])
+            elif v != parent[s]:
+                low[s] = min(low[s], time_arr[v])
 
-            if u == parent[s]:
-                continue
+    for v in range(n):
+        if not visited[v]:
+            DFSVisit(G, v)
+    
+    for i in range(n):
+        if time_arr[i] == low[i] and parent[i] != None:
+            print(parent[i], i)
 
-            if visited[u]:
-                low[s] = min(low[s], time_arr[u])
 
-            if not visited[u]:
-                parent[u] = s
-                DFSVisit(G, u)
-                low[s] = min(low[s], low[u])
-                if low[u] > time_arr[s]:
-                    print(s, u)
+
+# def bridge(G):
+#     n = len(G)
+#     visited = [False for v in range(n)]
+#     parent = [None for v in range(n)]
+#     time_arr = [-1 for v in range(n)]
+#     low = [-1 for v in range(n)]
+#     time = 0
+
+
+#     def DFSVisit(G, s):
+#         nonlocal time
+#         visited[s] = True
+#         time_arr[s] = time
+#         low[s] = time
+#         time += 1
+
+#         for u in G[s]:
+
+#             if u == parent[s]:
+#                 continue
+
+#             if visited[u]:
+#                 low[s] = min(low[s], time_arr[u])
+
+#             if not visited[u]:
+#                 parent[u] = s
+#                 DFSVisit(G, u)
+#                 low[s] = min(low[s], low[u])
+#                 if low[u] > time_arr[s]:
+#                     print(s, u)
                 
 
-    for i in range(n):
-        if not visited[i]:
-            DFSVisit(G, i)
-    # print(parent)
-    # print(time_arr)
-    # print(low)
+#     for i in range(n):
+#         if not visited[i]:
+#             DFSVisit(G, i)
+#     # print(parent)
+#     # print(time_arr)
+#     # print(low)
 
 bridge(G)
