1afd9a7e5256c5c8d8843d5f252be1865c75b242
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-05-17 23:17:11 +0200

    Create kruskal.py

---
+++ Graph algorithms/kruskal.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.parent = self
+        self.rank = 0
+        self.value = value
+
+def find(v):
+    if v.parent != v:
+        v.parent = find(v.parent)
+    return v.parent
+    
+def union(u, v):
+    u = find(u)
+    v = find(v)
+
+    if v.rank > u.rank:
+        u.parent = v
+    else:
+        v.parent = u
+        if v.rank == u.rank:
+            v.rank += 1
+
+def edges(G):
+    E = []
+    for i in range(len(G)):
+        for j in range(len(G[i])):
+            if ((G[i][j][0], i ,G[i][j][2])) not in E:
+                E.append(i, (G[i][j][0] ,G[i][j][2]))
+    return E
+
+def kruskal(G):
+    E = edges(G)
+    E = E.sort(key= lambda x : x[2])
+    V = []
+    MST = []
+    for i in range(len(G)):
+        V.append(Node(i))
+
+    for u, v, w in E:
+        if find(v) != find(u):
+            MST.append((u, v, w))
+            union(V[u], V[v])
+
+    return MST
