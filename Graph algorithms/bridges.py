a66a126db9d4fe01da5cd541c2a6a8cfd7dd14f5
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-05-23 23:49:16 +0200

    Update bridges.py

--- Graph algorithms/bridges.py
+++ Graph algorithms/bridges.py
@@ -11,8 +11,21 @@
 #     [0,0,0,0,0,1,0,1],
 #     [0,0,0,0,0,1,1,0]]
 # G = [[1, 2], [0, 4], [0, 3, 4], [2], [1, 2, 5], [4, 6, 7], [5, 7], [5, 6]]
-G =  [[1, 2], [0, 3], [0, 4], [1, 5, 6], [2, 7], [3, 8], [3, 8], [4, 8], [5, 6, 7, 9], [8, 10, 11], [9, 12], [9, 12], [10, 11]]
+# G =  [[1, 2], [0, 3], [0, 4], [1, 5, 6], [2, 7], [3, 8], [3, 8], [4, 8], [5, 6, 7, 9], [8, 10, 11], [9, 12], [9, 12], [10, 11]]
+G = [[0,1,1,0,0,0],
+     [1,0,1,0,0,0],
+     [1,1,0,1,0,1],
+     [0,0,1,0,1,0],
+     [0,0,0,1,0,1],
+     [0,0,1,0,1,0]]
 
+def make_graph(G):
+    new_graph = [[] for _ in range(len(G))]
+    for i in range(len(G)):
+        for j in range(len(G)):
+            if G[i][j] == 1:
+                new_graph[i].append(j)
+    return new_graph
 
 def bridge(G):
     n = len(G)
@@ -46,44 +59,15 @@ def bridge(G):
             print(parent[i], i)
 
 
-
-# def bridge(G):
-#     n = len(G)
-#     visited = [False for v in range(n)]
-#     parent = [None for v in range(n)]
-#     time_arr = [-1 for v in range(n)]
-#     low = [-1 for v in range(n)]
-#     time = 0
-
-
-#     def DFSVisit(G, s):
-#         nonlocal time
-#         visited[s] = True
-#         time_arr[s] = time
-#         low[s] = time
-#         time += 1
-
-#         for u in G[s]:
-
-#             if u == parent[s]:
-#                 continue
-
-#             if visited[u]:
-#                 low[s] = min(low[s], time_arr[u])
-
-#             if not visited[u]:
-#                 parent[u] = s
-#                 DFSVisit(G, u)
-#                 low[s] = min(low[s], low[u])
-#                 if low[u] > time_arr[s]:
-#                     print(s, u)
-                
-
-#     for i in range(n):
-#         if not visited[i]:
-#             DFSVisit(G, i)
-#     # print(parent)
-#     # print(time_arr)
-#     # print(low)
-
-bridge(G)
+print(make_graph(G))
+# bridge(make_graph(G))
+bridge([
+    [1, 2],
+    [0, 2],
+    [0, 1, 6],
+    [5, 4],
+    [5, 3],
+    [3,4,6],
+    [2,7,5],
+    [6]
+    ])
