6589dbff3724546ca6c59b004520d2261979c0fa
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-06-29 12:55:45 +0200

    added nlogn solution

--- Offline tasks/09_dynamic.py
+++ Offline tasks/09_dynamic.py
@@ -1,5 +1,6 @@
 from zad9testy import runtests
 
+# n^2
 def min_cost( O, C, T, L ):
  
     oc = list(zip(O, C))
@@ -23,6 +24,58 @@ def min_cost( O, C, T, L ):
                 break
                 
     return f1[n-1]
+
+def min_cost_nlogn( O, C, T, L ):
+ 
+    oc = [(o, c) for o, c in zip(O, C)]
+    oc.append((0, 0))
+    oc.append((L, 0))
+    oc.sort()
+
+    n = len(oc)
+    F0 = [float('inf') for _ in range(n)]
+    F1 = [float('inf') for _ in range(n)]
+    F0[0], F1[0] = 0, 0
+    
+    Q0 = PriorityQueue()
+    Q1 = PriorityQueue()
+    Q1_2T = PriorityQueue()
+    
+    Q0.put((0, 0))
+    Q1.put((0, 0))
+    Q1_2T.put((0, 0))
+    
+    for i in range(1, n):
+
+        while True:
+            m1 = Q1.get()
+            if oc[i][0] - m1[1] <= T:
+                break
+        Q1.put(m1)        
+
+        while True:
+            m1_2T = Q1_2T.get()
+            if oc[i][0] - m1_2T[1] <= 2*T:
+                break
+        Q1_2T.put(m1_2T)
+
+        while True:
+            m0 = Q0.get()
+            if oc[i][0] - m0[1] <= T:
+                break
+        Q0.put(m0)
+
+        F0[i] = m0[0] + oc[i][1]
+        F1[i] = min(m1[0], m1_2T[0]) + oc[i][1]
+
+        Q0.put((F0[i], oc[i][0]))
+        Q1_2T.put((F0[i], oc[i][0]))
+        Q1.put((F1[i], oc[i][0]))
+
+    # print(F0)
+    # print(F1)
+
+    return F1[n-1]
 
 O = [17, 20, 11, 5, 12]
 C = [9, 7, 7, 7, 3]
