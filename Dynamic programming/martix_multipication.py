15efb1ccf130fcbfb8adb4e8224ccd5d696f7d81
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-07-01 00:13:26 +0200

    Update martix_multipication.py

--- Dynamic programming/martix_multipication.py
+++ Dynamic programming/martix_multipication.py
@@ -1,4 +1,4 @@
-
+# this algorithm calculate minimum cost of matrix chain multipication
 
 def min_mult_rek(dims, i, j):
     if i == j:
@@ -15,13 +15,6 @@ def min_mult_rek(dims, i, j):
             min = res
     return min
 
-A1 = [(1, 3), (3, 5)] # 15
-A2 = [(1, 3), (3, 5), (5, 7)] # 50
-A3 = [(40, 20), (20, 30), (30, 10), (10, 30)] # 26000
-A4 = [(10, 20), (20, 30), (30, 40), (40, 30)] # 30K
-A4 = [(2, 2), (2, 8), (8, 6), (6, 9), (9, 9), (9, 2), (2, 1), (1, 9), (9, 6)] # 287
-A5 = [(2, 2), (2, 8), (8, 6), (6, 9), (9, 9), (9, 2), (2, 1)] # 221
-
 def make_dims(A):
     dims = [0 for _ in range(len(A) + 1)]
     dims[0] = A[0][0]
@@ -30,4 +23,31 @@ def make_dims(A):
     dims[len(A)] = A[len(A) - 1][1]
     return dims
 
-print(min_mult_rek(make_dims(A2), 1, len(A2)))
+def min_mult_dp(A):
+    n = len(A)
+
+    dp = [[float('inf') for _ in range(n)] for _ in range(n)]
+    
+    for i in range(n):
+        dp[i][i] = 0 # that's for matrix multipication by itself
+        # when length of subsequence equals 1
+
+    for i in range(n - 1):
+        dp[i][i + 1] = A[i][0] * A[i][1] * A[i + 1][1] # (AB), (BC), (CD)...
+    
+    for j in range(2, n): # length of the product, starting from length 3
+        for i in range(n - j): # starting index
+            for k in range(i, i + j): # checking each possibilities
+                dp[i][i + j] = min(dp[i][i + j], dp[i][k] + dp[k + 1][i + j] + A[i][0] * A[k][1] * A[i + j][1])
+
+    print(*dp, sep='\n')
+    return dp[0][n - 1]
+
+A1 = [(1, 3), (3, 5)] # 15
+A2 = [(1, 3), (3, 5), (5, 7)] # 50
+A3 = [(40, 20), (20, 30), (30, 10), (10, 30)] # 26000
+A4 = [(10, 20), (20, 30), (30, 40), (40, 30)] # 30K
+A4 = [(2, 2), (2, 8), (8, 6), (6, 9), (9, 9), (9, 2), (2, 1), (1, 9), (9, 6)] # 287
+A5 = [(2, 2), (2, 8), (8, 6), (6, 9), (9, 9), (9, 2), (2, 1)] # 221
+# print(min_mult_rek(make_dims(A2), 1, len(A2)))
+print(min_mult_dp(A5))
