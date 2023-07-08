9c8dbb622aff24e1316ca3096409d6caf0e8894c
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-07-08 12:44:36 +0200

    djikstra

--- Graph algorithms/djikstra.py
+++ Graph algorithms/djikstra.py
@@ -1,4 +1,5 @@
 import heapq
+from queue import PriorityQueue
 # (v1, v2, weight) v1 < v2
 E = [(0, 1, 2), (0, 3, 8), (1, 3, 5), (1, 4, 6), (3, 4, 3), (3, 5, 2), (4, 5, 1), (2, 4, 9), (2, 5, 3)]
 
@@ -30,7 +31,39 @@ def Djikstra(E, n, s):
         done[u] = True
 
 
+
     print(dist)
     print(prev)
 
-Djikstra(E, 6, 0)
+def djiksstra(G, s, t):
+    n = len(G)
+    dist = [float('inf') for _ in range(n)]
+    prev = [None for _ in range(n)]
+    done = [False for _ in range(n)]
+    dist[s] = 0
+    done[s] = True
+    Q = PriorityQueue()
+    Q.put((0, s))
+
+    while not Q.empty():
+        w, v = Q.get()
+
+        for u, w_u in G[v]:
+            if not done[u] and dist[u] > w_u + dist[v]:
+                dist[u] = w_u + dist[v]
+                prev[u] = v
+                Q.put((dist[u], u))
+        done[v] = True
+        if done[t]:
+            return dist[t]
+    print(dist)
+    return dist[t]
+
+graph = [[(1, 1), (2, 12)],
+         [(0, 1), (2, 7), (3, 5)],
+         [(0, 12), (1, 2), (3, 6), (4, 2)],
+         [(1, 5), (2, 6), (4, 4), (5, 100)],
+         [(2, 2), (3, 4), (5, 9)],
+         [(3, 100), (4, 9)]]
+for i in range(len(graph)):
+    print(djiksstra(graph, 0, i))
--- Graph algorithms/djikstra_matrix.py
+++ Graph algorithms/djikstra_matrix.py
@@ -1,6 +1,6 @@
 import heapq
 
-def djikstra(G, s, t):
+def djikstra(G, s, t): 
     n = len(G)
     d = [float('inf') for v in range(n)]
     visited = [False for v in range(n)]
@@ -15,15 +15,44 @@ def djikstra(G, s, t):
         w_u, u = heapq.heappop(heap)
 
         for i in range(n):
-            if G[u][i] != 0 and not visited[i]:
+            if G[u][i] != -1 and not visited[i]:
                 if d[i] > d[u] + G[u][i]:
                     d[i] = d[u] + G[u][i]
                     heapq.heappush(heap, (d[i], i))
     return d
 
-G = [[0, 1, 5, 0, 0],
-         [1, 0, 2, 8, 7],
-         [5, 2, 0, 3, 0],
-         [0, 8, 3, 0, 1],
-         [0, 7, 0, 1, 0]]
+def djikstra_better(G, s):
+    n = len(G)
+    parent = [False for _ in range(n)]
+    dist = [float('inf') for _ in range(n)]
+    visited = [False for _ in range(n)]
+    dist[s] = 0
+
+    while True:
+        min_dist = float('inf')
+        min_u = -1
+
+        for u in range(n):
+            if not visited[u] and dist[u] < min_dist:
+                min_u = u
+                min_dist = dist[u]
+        if min_u == -1:
+            break
+        visited[min_u] = True
+        u = min_u
+
+        for v in range(n):
+            if not visited[v] and G[u][v] != -1 and dist[v] > dist[u] + G[u][v]:
+                dist[v] = dist[u] + G[u][v]
+                parent[v] = u
+    
+    return dist
+
+
+G = [[-1, 1, 5, -1, -1],
+    [1, -1, 2, 8, 7],
+    [5, 2, -1, 3, -1],
+    [-1, 8, 3, -1, 1],
+    [-1, 7, -1, 1, -1]]
 print(djikstra(G, 0, 1))
+print(djikstra_better(G, 0))
