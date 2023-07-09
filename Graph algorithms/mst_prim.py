63563f469944b29c2dad9db7df165d3b886a0a19
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2023-07-09 10:23:41 +0200

    Update mst_prim.py

--- Graph algorithms/mst_prim.py
+++ Graph algorithms/mst_prim.py
@@ -24,24 +24,24 @@ def prim(G, s):
 
 
 G = [[(1, 1), (3, 5), (5, 8)], [(0, 1), (2, 3)], [(1, 3), (3, 4), (4, 6)], [(0,5), (4, 2), (2, 4), (5, 7)], [(3, 2), (2, 6)], [(0, 8), (3, 7)]]
+prim(G, 2)
 
-n = 6
-B = [(0, 1, 1),
-     (0, 3, 8),
-     (0, 4, 5),
-     (1, 2, 3),
-     (2, 4, 4),
-     (2, 5, 6),
-     (3, 4, 7),
-     (4, 5, 2)
-     ]
-def graph(B, n):
-    G = [[] for _ in range(n)]
+# n = 6
+# B = [(0, 1, 1),
+#      (0, 3, 8),
+#      (0, 4, 5),
+#      (1, 2, 3),
+#      (2, 4, 4),
+#      (2, 5, 6),
+#      (3, 4, 7),
+#      (4, 5, 2)
+#      ]
+# def graph(B, n):
+#     G = [[] for _ in range(n)]
 
-    for kraw in B:
-        G[kraw[0]].append([kraw[1], kraw[2]])
-        G[kraw[1]].append([kraw[0], kraw[2]])
+#     for kraw in B:
+#         G[kraw[0]].append([kraw[1], kraw[2]])
+#         G[kraw[1]].append([kraw[0], kraw[2]])
 
-    return G
-G = graph(B, n)
-prim(G, 0)
+#     return G
+# G = graph(B, n)
